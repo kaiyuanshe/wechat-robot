11384afad06562d7b856f961a15df936eb22f3cd
庄表伟 <zhuangbiaowei@gmail.com>
2018-11-25 12:12:29 +0800

    完善项目介绍

--- dialog.js
+++ dialog.js
@@ -8,6 +8,12 @@ exports.getReply = function (msg) {
       return "1. KCoin：基于区块链的开源项目激励平台\n2. 开放黑客松：Open Hackathon Platform\n3. 开源社官网：基于Github Pages的开源社官方网站\n4. Wechat-Robot：开源社微信机器人\n#project 序号，了解更多";
     case "#project 1":
       return "KCoin，基于区块链（超级账本）技术，服务开源项目，实现快捷方便的开源贡献积分平台，并提供兑换机制，激励更多的开源贡献。项目地址：  https://github.com/kaiyuanshe/kcoin";
+    case "#project 2":
+      return "开放黑客松，基于云计算平台，组织者能够托管具有最需要功能的离线/在线黑客马拉松; 活动参与者可以使用支持HTML5的Web浏览器登录黑客松活动，提供他选择的开发环境并与队友协作。项目地址：  https://github.com/kaiyuanshe/open-hackathon";
+    case "#project 3":
+      return "开源社官网，以博客和新闻为主的静态网站。项目地址：  https://github.com/kaiyuanshe/kaiyuanshe";
+    case "#project 4":
+      return "开源社微信机器人，一个探索型项目，尝试基于微信机器人，与更多的开源爱好者沟通，并提高吸纳新成员的效率。项目地址：  https://github.com/kaiyuanshe/wechat-robot";
     default:
       return "输入help可以查看如何与我交流。";
   }

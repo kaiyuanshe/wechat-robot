e38a6c8ceff90e9f4f7dda4a066ee50c2ce5b1bc
Zhuang BiaoWei <zhuangbiaowei@huawei.com>
2018-12-22 17:01:24 +0800

    添加保存消息的处理函数

--- bot.js
+++ bot.js
@@ -39,13 +39,14 @@ async function onMessage(msg) {
     }
     if (msg.payload) {
         if (msg.room() != null && msg.payload.type != bot.Message.Type.Unknown) {
-	    var room = await msg.room();
-            console.log(await room.topic()+":"+room.id);
+            var room = await msg.room();
+            console.log(await room.topic() + ":" + room.id);
             GitterUtils.sendMsgToGitter(bot, msg);
             CommandUtils.do_room_command(bot, msg);
         } else if (msg.payload.type != bot.Message.Type.Unknown && msg.from().name() != "开源社-bot") {
-	    CommandUtils.do_user_command(bot, msg);
+            CommandUtils.do_user_command(bot, msg);
         }
+        DBUtils.save_msg(msg);
     }
 }
 
@@ -54,10 +55,10 @@ async function onFriendship(friendship) {
     if (friendship.type() == bot.Friendship.Type.Receive) {
         await friendship.accept();
     } else if (friendship.type() == bot.Friendship.Type.Confirm) {
-	var contact = await friendship.contact();
-	await contact.sync();
+        var contact = await friendship.contact();
+        await contact.sync();
         contact.say(Dialog.greeting);
-	DBUtils.save_wechat_friend(contact);
+        DBUtils.save_wechat_friend(contact);
     }
 }
 
@@ -73,43 +74,43 @@ bot.start()
 
 const kue = require('kue');
 const queue = kue.createQueue();
-queue.process("UserApply", 1, async function(job, done){
-  var work_group = job.data.work_group;
-  var room_id = RoomID[work_group];
-  var room = await bot.Room.load(room_id);
-  if(room){
-    room.sync();
-    var text = "有新人申请加入："+job.data.nick_name +"\n"+"申请加入的小组："+job.data.work_group+"\n"+"申请理由与自我介绍："+job.data.introduce+"\n";
-    text = text + "组长可以@机器人，并发布命令：“接纳@"+job.data.nick_name+"” 或 “同意@"+job.data.nick_name+"” ，机器人将会把他拉入本群。\n";
-    text = text + "如果组长@机器人，并发布命令：“正式@"+job.data.nick_name+"”，机器人将会同时把他拉入本群与开源社正式成员群。";
-    await room.say(text);
-  }
-  if (job.data.referee1 && job.data.referee2) {
-    room_id = RoomID["成员发展工作组"];
-    room = await bot.Room.load(room_id);
-    var text = "有新人申请加入：" + job.data.nick_name + "\n" +
-	       "申请加入的小组：" + job.data.work_group + "\n" +
-	       "申请理由与自我介绍：" + job.data.introduce + "\n" +
-	       "推荐人：" + job.data.referee1 + " 和 " + job.data.referee2;
-    if (room){
-      await room.say(text);
+queue.process("UserApply", 1, async function (job, done) {
+    var work_group = job.data.work_group;
+    var room_id = RoomID[work_group];
+    var room = await bot.Room.load(room_id);
+    if (room) {
+        room.sync();
+        var text = "有新人申请加入：" + job.data.nick_name + "\n" + "申请加入的小组：" + job.data.work_group + "\n" + "申请理由与自我介绍：" + job.data.introduce + "\n";
+        text = text + "组长可以@机器人，并发布命令：“接纳@" + job.data.nick_name + "” 或 “同意@" + job.data.nick_name + "” ，机器人将会把他拉入本群。\n";
+        text = text + "如果组长@机器人，并发布命令：“正式@" + job.data.nick_name + "”，机器人将会同时把他拉入本群与开源社正式成员群。";
+        await room.say(text);
     }
-  }
-  done();
+    if (job.data.referee1 && job.data.referee2) {
+        room_id = RoomID["成员发展工作组"];
+        room = await bot.Room.load(room_id);
+        var text = "有新人申请加入：" + job.data.nick_name + "\n" +
+            "申请加入的小组：" + job.data.work_group + "\n" +
+            "申请理由与自我介绍：" + job.data.introduce + "\n" +
+            "推荐人：" + job.data.referee1 + " 和 " + job.data.referee2;
+        if (room) {
+            await room.say(text);
+        }
+    }
+    done();
 });
 
-queue.process("AddFriend", 1, async function(job, done){
-  var wechat_id = job.data;
-  console.log(wechat_id);
-  var contact = bot.Contact.load(wechat_id);
-  await bot.Friendship.add(contact, "test add friend");
-  done();
+queue.process("AddFriend", 1, async function (job, done) {
+    var wechat_id = job.data;
+    console.log(wechat_id);
+    var contact = bot.Contact.load(wechat_id);
+    await bot.Friendship.add(contact, "test add friend");
+    done();
 });
 
-queue.process("AllFriend", 1, async function(job, done){
-  const list = await bot.Contact.findAll();
-  list.forEach(function(item,index){
-    DBUtils.save_wechat_friend(item);
-  });
-  done();
+queue.process("AllFriend", 1, async function (job, done) {
+    const list = await bot.Contact.findAll();
+    list.forEach(function (item, index) {
+        DBUtils.save_wechat_friend(item);
+    });
+    done();
 });
--- db-utils.js
+++ db-utils.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const db = mysql.createConnection ({
+const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'password',
@@ -16,13 +16,13 @@ db.connect((err) => {
 global.db = db;
 
 exports.get_user = function (nick_name, func) {
-  if(nick_name.slice(0,1)=="@"){
+  if (nick_name.slice(0, 1) == "@") {
     nick_name = nick_name.slice(1);
   }
   let query = "select * from users where `nick_name`='" + nick_name + "'";
   console.log(query);
   db.query(query, (err, result) => {
-    if(!err){
+    if (!err) {
       func(result[0]);
     } else {
       func(null);
@@ -31,29 +31,68 @@ exports.get_user = function (nick_name, func) {
 }
 
 exports.update_user_status = function (wechat_id, user_status, func) {
-  let query = "update `users` set `status`='"+user_status+"' where `wechat_id`='"+wechat_id+"'";
+  let query = "update `users` set `status`='" + user_status + "' where `wechat_id`='" + wechat_id + "'";
   db.query(query, (err, result) => {
-    if(func) { func(); }
+    if (func) { func(); }
   });
 }
 
-exports.update_user_position = function (wechat_id, position, func){
-  let query = "update `users` set `position`='"+position+"' where `wechat_id`='"+wechat_id+"'";
+exports.update_user_position = function (wechat_id, position, func) {
+  let query = "update `users` set `position`='" + position + "' where `wechat_id`='" + wechat_id + "'";
   db.query(query, (err, result) => {
-    if(func) { func(); }
+    if (func) { func(); }
   });
 }
 
 
-exports.save_wechat_friend = async function (user){
+exports.save_wechat_friend = async function (user) {
   var wechat_id = user.id;
   var nick_name = await user.name();
-  console.log(wechat_id+","+nick_name);
+  console.log(wechat_id + "," + nick_name);
   db.query("SET NAMES utf8mb4", (err, result) => {
-    db.query("insert into `wechat_friends` (wechat_id,nick_name) values ('"+wechat_id+"','"+nick_name+"')", (err1, result1) => {
-      if (err1){
+    db.query("insert into `wechat_friends` (wechat_id,nick_name) values ('" + wechat_id + "','" + nick_name + "')", (err1, result1) => {
+      if (err1) {
         console.log(err1);
       }
     });
+  });
+}
+
+const MessageType = ["Unknown", "Attachment", "Audio", "Contact", "Emoticon", "Image", "Text", "Video", "Url"];
+
+exports.save_msg = async function (msg) {
+  var fields = "`type`,";
+  var values = "'" + MessageType[msg.type()] + "',";
+  var room = await msg.room();
+  if (room) {
+    var room_id = room.id;
+    var room_topic = await room.topic();
+    fields = fields + "`room_id`,`room_topic`,";
+    values = values + "'" + room_id + "','" + room_topic + "',";
+  }
+  var from = await msg.from();
+  if (from) {
+    var from_user_id = from.id;
+    var from_user_name = await from.name();
+    fields = fields + "`from_user_id`,`from_user_name`,";
+    values = values + "'" + from_user_id + "','" + from_user_name + "',";
+  }
+  var mention_list = await msg.mention();
+  if (mention_list) {
+    var mention_id_list = "";
+    var mention_name_list = "";
+    mention_list.forEach(function (item, index) {
+      mention_id_list = mention_id_list + item.id + ",";
+      mention_name_list = mention_name_list + await item.name() + ",";
+    });
+    fields = fields + "`mention_id_list`,`mention_nane_list`,";
+    values = values + "'" + mention_id_list + "','" + mention_name_list + "',";
+  }
+  text = await msg.text();
+  fields = fields + "`text`,`create_at`";
+  values = values + "'" + text + "',CURRENT_TIMESTAMP";
+  sql = "INSERT INTO `messages` (" + fields + ") VALUES (" + values + ")";
+  db.query("SET NAMES utf8mb4", (err, result) => {
+    db.query(sql);
   });
 }
